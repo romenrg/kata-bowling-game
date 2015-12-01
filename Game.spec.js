f9a02f66590dc6eff7602be605d7ac3f1700a20d
Romén Rodríguez <romenrg@gmail.com>
2015-12-01 16:18:37 +0000

    Adding a new test in which we expect the score to be that of the roll if we have only rolled once

--- Game.spec.js
+++ Game.spec.js
@@ -3,8 +3,14 @@ let Game = require("./Game");
 let expect = require('chai').expect;
 
 describe('Bowling Game', function() {
-    it('Initially the score is 0', function() {
+    it('Initially the score should be 0', function() {
         let game = new Game();
         expect(game.score()).to.equal(0);
+    });
+
+    it('Only having rolled once, the score must be the number of pins knocked down by that roll', function() {
+        let game = new Game();
+        game.roll(5);
+        expect(game.score()).to.equal(5);
     })
 });
