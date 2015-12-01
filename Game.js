200942d48fe8e41ca100b926d2e577092fe316ed
Romén Rodríguez <romenrg@gmail.com>
2015-12-01 18:07:54 +0000

    Test in which we expect the score to be that of the roll if we have only rolled once is passing

--- Game.js
+++ Game.js
@@ -1,17 +1,21 @@
 "use strict";
 
 class Game {
+    constructor() {
+        this.totalPins = 0;
+    }
     rollAll(framesListWithScores) {
         var framesArray = JSON.parse(framesListWithScores);
         return Array.isArray(framesArray);
     }
 
     roll(pins) {
+        this.totalPins += pins;
         return true;
     }
 
     score() {
-        return 0;
+        return this.totalPins;
     }
 }
 
