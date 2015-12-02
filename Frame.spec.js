81c9d8988e44d7bc74bce527f1366de25a55e387
Romén Rodríguez <romenrg@gmail.com>
2015-12-02 12:01:43 +0000

    Refactoring Frame tests to include a beforeEach statement to create a new class instance before each test

--- Frame.spec.js
+++ Frame.spec.js
@@ -3,36 +3,34 @@ let Frame = require("./Frame");
 let expect = require("chai").expect;
 
 describe("Bowling Game - Frame class", function() {
+    let frame;
+    beforeEach(function() {
+        frame = new Frame();
+    });
     it('Initially the score should be 0', function() {
-        let frame = new Frame();
         expect(frame.score()).to.equal(0);
     });
     it('Only having rolled once, the score must be the number of pins knocked down by that roll', function() {
-        let frame = new Frame();
         frame.roll(5);
         expect(frame.score()).to.equal(5);
     });
     it('Rolling two times but not reaching 10, the score must be the addition', function() {
-        let frame = new Frame();
         frame.roll(5);
         frame.roll(2);
         expect(frame.score()).to.equal(7);
     });
     it('A spare was achieved in this frame and the next ball knocked down 2 pins', function() {
-        let frame = new Frame();
         frame.roll(4);
         frame.roll(6);
         frame.bonus(2);
         expect(frame.score()).to.equal(12);
     });
     it('A strike was achieved in this frame and the next two balls knocked down 8 pins', function() {
-        let frame = new Frame();
         frame.roll(10);
         frame.bonus(8);
         expect(frame.score()).to.equal(18);
     });
     it('Bonus should not be added if neither a spare nor a strike was achieved in this frame', function() {
-        let frame = new Frame();
         frame.roll(3);
         frame.roll(3);
         frame.bonus(8);
