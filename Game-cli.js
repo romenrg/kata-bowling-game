c91e5f85685e8cc11a860893473ebd2c1aa2ac94
Romén Rodríguez <romenrg@gmail.com>
2015-12-02 14:13:23 +0000

    Finishing the cli for the Kata

--- Game-cli.js
+++ Game-cli.js
@@ -7,7 +7,9 @@ console.log("---------------------------------");
 var argv = process.argv.slice(2);
 var errorMessage = "ERROR: List of frames with their corresponding scores is required (e.g. [[3, 4], [5, 2], [10]...]";
 if (argv.length == 1) {
-    console.log((new Game()).rollAll(argv[0]));
+    var game = new Game();
+    game.rollAll(argv[0]);
+    console.log(game.score());
 }
 else {
     console.log(errorMessage);
